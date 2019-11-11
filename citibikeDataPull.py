a8b749c0ebe23ca2f371362dd4b36a3f50208bd3
DavidBakker <david.bakker@relocationinsurance.com>
2019-11-11 14:10:43 -0500

    commit 2

--- citibikeDataPull.py
+++ citibikeDataPull.py
@@ -8,11 +8,7 @@ import requests
 import time
 
 import pandas as pd
-import numpy as np
-import shelve
 from selenium import webdriver
-from sqlalchemy import create_engine
-#from settings import DB_CONNECTION
 
 
 def get_links_from_page(source):
@@ -22,48 +18,50 @@ def get_links_from_page(source):
     time.sleep(10)    #allow time for page scripts to execute
     elem = browser.find_elements_by_tag_name('a')
     links = [i.get_attribute('href') for i in elem]
-    links.remove(source)              
+    links.remove(source)
     return links
-
-
-sourceURL = 'https://s3.amazonaws.com/tripdata/index.html' 
-allLinks = get_links_from_page(sourceURL)
-recentLinks = [i for i in allLinks if os.path.basename(i).startswith('2018')]
 
 
 def import_data_to_dataframe(files):
     dataFrame = pd.DataFrame()
     for i in files:
         try:
-            print('Opening ' + i + '...')
-            newDf = pd.read_csv(i, index_col=0)
+            print('Opening ' + os.path.basename(i) + '...')
+            newDf = pd.read_csv(i, index_col=False)
             print('Appending data...')
             dataFrame = pd.concat([dataFrame, newDf])
         except Exception as err:
             print('***ERROR*** ' +str(err))
+    print('Import Complete.')
     return dataFrame
 
 
-df = import_data_to_dataframe(recentLinks)
-print('Import complete.')
+def clean_data(dataframe):
+    print('Cleaning data.')
+    dataframe = dataframe.dropna()
+    dataframe = dataframe.drop_duplicates()
+    dataframe.columns = dataframe.columns.str.replace(' ', '_')
+    dataframe[['usertype', 'gender']].apply(lambda x: x.astype('category'))
+    dataframe[['starttime', 'stoptime']] = df[['starttime', 'stoptime']].apply(pd.to_datetime)
+    assert dataframe.notnull().all().all()
+    return dataframe
 
 
-df.columns = df.columns.str.replace(' ', '_')
-df[['usertype', 'gender']].apply(lambda x: x.astype('category'))
-df[['starttime', 'stoptime']] = df[['starttime', 'stoptime']].apply(pd.to_datetime)
-df[['start_station_id', 'end_station_id']].apply(lambda x: x.fillna(0).astype(int))
+def quick_df_facts(dataframe):
+    print('Quick facts:')
+    print('\n\nTop 5\n')
+    print(dataframe.head())
+    print('\n\nTypes\n')
+    print(dataframe.info())
+    print('\n\nDescribe \n')
+    print(dataframe.describe())
 
 
-
-print(df.head())
-print('\n\n')
-print(df.info())
-print('\n\n')
-print(df.describe())
-
-df.to_csv('.\\citibikedata.csv')
-
-
-
-
-print('Done')
+if __name__ == '__main__':
+    sourceURL = 'https://s3.amazonaws.com/tripdata/index.html' 
+    allLinks = get_links_from_page(sourceURL)
+    recentLinks = [i for i in allLinks if os.path.basename(i).startswith('2018')]
+    df = import_data_to_dataframe(recentLinks)
+    df = clean_data(df)
+    quick_df_facts(df)
+    
